701c55a353595dd7abcbad3e6b3713833553b0cb
David Vargas <dvargas92495@gmail.com>
2022-05-16 16:56:31 -0500

    Edit remix config and move dir

--- remix.config.js
+++ remix.config.js
@@ -4,9 +4,8 @@
 module.exports = {
   appDirectory: "app",
   assetsBuildDirectory: "public/build",
-  publicPath: "/build/",
-  serverBuildDirectory: "server/build",
   devServerPort: 8002,
   ignoredRouteFiles: [".*"],
-//   serverDependenciesToBundle: ["react-force-graph-2d", "force-graph", /^d3-/, "internmap"],
+  publicPath: "/build/",
+  serverBuildPath: "app/server/build/index.js",
 };
