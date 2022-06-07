dc4a4c0873e245c8df3a6fc6da2368a1bafdf37d
David Vargas <dvargas92495@gmail.com>
2022-06-07 14:45:17 -0400

    Buffer concat

--- app/data/uploadJson.server.ts
+++ app/data/uploadJson.server.ts
@@ -3,15 +3,17 @@ import type { UploadHandler } from "@remix-run/node";
 
 const s3 = new AWS.S3({ region: process.env.AWS_REGION });
 
-const uploadJson: UploadHandler = ({ filename, data, ...rest }) => {
-  console.log("rest", rest);
-  console.log("data", data);
+const uploadJson: UploadHandler = async ({ filename, data }) => {
+  let Body = [];
+  for await (const datum of data) {
+    Body.push(datum);
+  }
   const Key = `data/${filename}`;
   return s3
     .upload({
       Bucket: "clarity.davidvargas.me",
       Key,
-      Body: data,
+      Body: Buffer.concat(Body),
     })
     .promise()
     .then(() => Key);
